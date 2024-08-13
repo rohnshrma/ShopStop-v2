ed0c192cd29945a78071c7bfb746d625c0c12e60
rohnshrma <rohn11shrma@gmail.com>
2024-08-13 13:14:46 +0530

    section 4 DONE

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+const port = process.env.PORT || 4000;
+
+connectDB();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API is Running.");
+});
+
+app.use("/api/products", productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log("server started on port", port);
+});
